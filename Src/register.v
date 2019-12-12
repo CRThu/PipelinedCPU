bf0dbbe5f0388c5bc7246b97e251ae4da4394ce1
yummycarrot <yummycarrot@qq.com>
2019-12-12 14:17:50 +0800

    tested SLL,SRL and fix a bug

--- Src/register.v
+++ Src/register.v
@@ -3,10 +3,10 @@ module register(
         input   wire            reset_n,
 
         input   wire    [4:0]   addr1,
-        output  wire    [31:0]  read1,
+        output  reg     [31:0]  read1,
 
         input   wire    [4:0]   addr2,
-        output  wire    [31:0]  read2,
+        output  reg     [31:0]  read2,
 
         input   wire            we3,
         input   wire    [4:0]   addr3,
@@ -15,9 +15,40 @@ module register(
 
     reg [31:0] register_block [31:0];
 
-    assign read1 = (addr1==5'b00000) ? 31'h0 : register_block[addr1];
-    assign read2 = (addr2==5'b00000) ? 31'h0 : register_block[addr2];
-
+    /*  Read  */
+    //assign read1 = (addr1==5'b00000) ? 32'h0 : register_block[addr1];
+    //assign read2 = (addr2==5'b00000) ? 32'h0 : register_block[addr2];
+    always@(*)
+    begin
+        if(addr1 == 5'b00000)
+        begin
+            read1 = 32'h0;
+        end
+        else
+        if(addr1 == addr3 && we3)
+        begin
+            read1 = write3;
+        end
+        else
+        begin
+            read1 = register_block[addr1];
+        end
+        
+        if(addr2 == 5'b00000)
+        begin
+            read2 = 32'h0;
+        end
+        else
+        if(addr2 == addr3 && we3)
+        begin
+            read2 = write3;
+        end
+        else
+        begin
+            read2 = register_block[addr1];
+        end
+    end
+    
     integer i;
     always @(posedge clk or negedge reset_n)
     begin
